cfbeae01d0f9712db4d12424eb1f7fc56c0c8212
kevinlee05 <kevinlee05@gmail.com>
2018-02-17 17:38:57 +0800

    extract url

--- csintro/getlinks.py
+++ csintro/getlinks.py
@@ -3,8 +3,12 @@
 # where the first '<a href=' occurs in a page.
 
 page = '''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="/">'''
+   <div class="udacity float-left"> <a href="/sample/">'''
 
 start_link = page.find('<a href=')
+start_quote = page.find('\"',start_link)
+start_url = start_quote+1
+end_quote = page.find('\"', start_quote+1)
 
-print(start_link)
+url = page[start_url:end_quote]
+print(url)
