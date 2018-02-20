a177a188a85a9a06e9a00fd57eecbb7db2eadaf2
kevinlee05 <kevinlee05@gmail.com>
2018-02-20 11:45:19 +0800

    max_depth, max_pages, sudoku, total_enrollment

---
+++ csintro/unit3/sudoku.py
@@ -0,0 +1,112 @@
+# THREE GOLD STARS
+
+# Sudoku [http://en.wikipedia.org/wiki/Sudoku]
+# is a logic puzzle where a game
+# is defined by a partially filled
+# 9 x 9 square of digits where each square
+# contains one of the digits 1,2,3,4,5,6,7,8,9.
+# For this question we will generalize
+# and simplify the game.
+
+# Define a procedure, check_sudoku,
+# that takes as input a square list
+# of lists representing an n x n
+# sudoku puzzle solution and returns the boolean
+# True if the input is a valid
+# sudoku square and returns the boolean False
+# otherwise.
+
+# A valid sudoku square satisfies these
+# two properties:
+
+#   1. Each column of the square contains
+#       each of the whole numbers from 1 to n exactly once.
+
+#   2. Each row of the square contains each
+#       of the whole numbers from 1 to n exactly once.
+
+# You may assume the the input is square and contains at
+# least one row and column.
+
+correct = [[1,2,3],
+           [2,3,1],
+           [3,1,2]]
+
+incorrect = [[1,2,3,4],
+             [2,3,1,3],
+             [3,1,2,3],
+             [4,4,4,4]]
+
+incorrect2 = [[1,2,3,4],
+             [2,3,1,4],
+             [4,1,2,3],
+             [3,4,1,2]]
+
+incorrect3 = [[1,2,3,4,5],
+              [2,3,1,5,6],
+              [4,5,2,1,3],
+              [3,4,5,2,1],
+              [5,6,4,3,2]]
+
+incorrect4 = [['a','b','c'],
+              ['b','c','a'],
+              ['c','a','b']]
+
+incorrect5 = [ [1, 1.5],
+               [1.5, 1]]
+
+def check_sudoku(list):
+    rowcheck = []
+    columncheck = []
+    columns = len(list[0])
+    rows = len(list)
+    print(columns, rows)
+    if not columns == rows:
+        return False
+    offset = 0
+    for row in list:
+        for i in row:
+            if not i in range(1, rows + 1):
+                return False
+            if i in rowcheck:
+                return False
+            if i > rows:
+                return False
+            rowcheck.append(i)
+        rowcheck = []
+    for j in range(1, columns+1):
+        for i in range(1, rows+1):
+            element = list[i-1][j-1]
+            if not element in range(1, rows + 1):
+                return False
+            if element in columncheck:
+                return False
+            if element > columns:
+                return False
+            columncheck.append(element)
+            #print(columncheck)
+        columncheck=[]
+    return True
+
+
+
+
+#print check_sudoku(incorrect)
+#>>> False
+
+#print check_sudoku(correct)
+#>>> True
+
+#print check_sudoku(incorrect2)
+#>>> False
+
+#print check_sudoku(incorrect3)
+#>>> False
+
+#print check_sudoku(incorrect4)
+#>>> False
+
+#print check_sudoku(incorrect5)
+#>>> False
+
+
