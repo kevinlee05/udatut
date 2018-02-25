2df07bcb713f9c64ae0dde3f30128e60da5bd15f
kevinlee05 <kevinlee05@gmail.com>
2018-02-26 00:18:20 +0800

    hash tables

--- dsalgos/searchandsort/quicksort.py
+++ dsalgos/searchandsort/quicksort.py
@@ -4,8 +4,8 @@ Output a sorted list."""
 def quicksort(array):
     left = 0
     right = len(array) -1
-    pivot = (left + right) /2
-    # print(array, left ,right, pivot)
+    pivot = int((left + right) /2)
+    print(array, left, pivot, right)
     sortpartition(array, left, right, pivot)
     return array
 
@@ -17,7 +17,7 @@ def sortpartition(array, left, right, pivot):
     if pivot == left or pivot == right:
         return
     tempright = right
-    for i in range(left, pivot + 1):
+    for i in range(left, int(pivot + 1)):
         if array[i] > array[tempright]:
             array[i], array[tempright] = array[tempright], array[i]
         if array[i] > array[pivot]:
@@ -26,9 +26,12 @@ def sortpartition(array, left, right, pivot):
             array[tempright], array[pivot] = array[pivot], array[tempright]
         tempright -= 1
         i += 1
-    # print(array, left ,right, pivot)
-    sortpartition(array, left, pivot, (left+pivot)/2 )
-    sortpartition(array, pivot, right, (right+pivot)/2 )
+    print(array, left , pivot, right)
+    sortpartition(array, left, pivot, int((left+pivot)/2 ))
+    sortpartition(array, pivot, right, int((right+pivot)/2) )
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-print quicksort(test)
+print(quicksort(test))
+test2 = [21, 4, 1, 405, 54, 9, 20, 25, 6, 2, 14,8, 104, 29]
+print(quicksort(test2))
+
